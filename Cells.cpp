f1c4ee8d9893ccfefc8531c69772c65c1c85b67b
KingGreil <r.dumerle@orange.fr>
2025-12-04 11:49:37 +0100

    GetPosUi déplacé+fix chemin font

--- Cells.cpp
+++ Cells.cpp
@@ -1,23 +1,15 @@
 #include "Cells.h"
 
-Cell::Cell(int _x, int _y, int al) {
+Cell::Cell(int _x, int _y, bool al) {
 	this->x = _x;
 	this->y = _y;
 	this->alive = al;
 
 	shape.setSize(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-	shape.setPosition(sf::Vector2f(static_cast<float>(getPosXUi()), static_cast<float>(getPosYUi())));
+	shape.setPosition(sf::Vector2f(static_cast<float>(getPosX()*CELL_SIZE), static_cast<float>(getPosY()*CELL_SIZE)));
 	shape.setFillColor(sf::Color::Black); // cell morte
 	shape.setOutlineThickness(1);
 	shape.setOutlineColor(sf::Color(150, 150, 150));
-}
-
-int Cell::getPosXUi(){
-	return (this->x * CELL_SIZE);
-}
-
-int Cell::getPosYUi() {
-	return (this->y * CELL_SIZE);
 }
 
 int Cell::getPosX() {
